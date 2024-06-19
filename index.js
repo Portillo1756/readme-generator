f9214cef56e8c2c483984a301d02504032edf908
porti@Portillo17 <portillo_andres17@yahoo.com>
2024-06-18 22:41:37 -0400

    created the questionar

--- index.js
+++ index.js
@@ -1,7 +1,50 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs");
+const inquirer = require('inquirer');
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+   {
+    type: "input",
+    name: "title",
+    message: "Name of your project?",
+   }
+
+   {
+    type: "input",
+    name: "description",
+    message: "Provide a description and purpose of your project.",
+   }
+
+   {
+    type: "input",
+    name: "usage",
+    message: "State the languages or technologies associated with this project.",
+   }
+
+   {
+    type: "checkbox",
+    name: "license",
+    message: "Select a license applicable to this project.",
+    choices: ["HTML", "CSS", "JavaScript", "Node.js"],
+   }
+   
+   {
+    type: "input",
+    name: "contributing",
+    message: "What is your GitHub username?",
+   }
+
+   {
+    type: "input",
+    name: "email",
+    messsage: "Provide a valid email address.",
+  }
+
+
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
