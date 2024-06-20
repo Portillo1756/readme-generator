f96953a8396df78657e90ce1630d09633c99bc7f
porti@Portillo17 <portillo_andres17@yahoo.com>
2024-06-19 20:54:46 -0400

    work on the question and the 4rd loops

--- index.js
+++ index.js
@@ -7,50 +7,67 @@ const generateMarkdown = require("./utils/generateMarkdown");
 const questions = [
    {
     type: "input",
-    name: "title",
+    name: "Title",
     message: "Name of your project?",
-   }
+   },
+
+  {
+    type: "input",
+    name: "Description",
+    message: "Provide a description the purpose of your project.",
+   },
 
    {
     type: "input",
-    name: "description",
-    message: "Provide a description and purpose of your project.",
-   }
-
-   {
-    type: "input",
-    name: "usage",
+    name: "Usage",
     message: "State the languages or technologies associated with this project.",
-   }
+    choices: ["HTML", "CSS", "JavaScript", "Node.js", "Phython", "JSON", "None"],
+   },
 
    {
     type: "checkbox",
-    name: "license",
+    name: "License",
     message: "Select a license applicable to this project.",
-    choices: ["HTML", "CSS", "JavaScript", "Node.js"],
-   }
+    
+   },
    
    {
     type: "input",
-    name: "contributing",
+    name: "Contributing",
     message: "What is your GitHub username?",
-   }
+   },
 
    {
     type: "input",
-    name: "email",
+    name: "Name",
+    message: "What Is Your Name?"
+   },
+
+   {
+    type: "input",
+    name: "Email",
     messsage: "Provide a valid email address.",
-  }
-
-
-
+  },  
+  
+  {
+    type: "input",
+    name: "Creator",
+    message: "What is your GitHub username?",
+   }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.createPromptModule(questions).then((response) => {
+    console.log("Creating Professional README.mD file...");
+    writeToFile("./dist/README.md", generateMarkdown({...response}));
+  })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,36 +1,72 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (licence !== "none") {
+    return `![Github license] (https://img.shields.io/badge/license ${license} yellowgreen.svg)`;
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "none") {
+    return `\n* [License](#license)\n`;
+}
+  return "";
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "none") {
+    return `## License
+    
+      Licensed under the ${license} license.`;
+  }
+  return "";
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+    return `# ${data.title}
+    by ${data.name}
+    ${renderLicenseBadge(data.licence)}
 
-  ## Description
+    #Table of Contents
+    * [Description](#Description)
+    * [Usage](#Usage)
+    * [License](#License)
+    * [Contact-Me](#Contact-Me)
+    * [Contributors](#Contributors)
+    * [Testing](Testing)
 
-  ${data.Description}
+    ## Description
 
-  ## Usage
+    ${data.Description}
 
-  ${data.Usage}
+    ## Usage
+
+    ${data.Usage}
 
 
-  ## License
+    ## License
 
-  ${data.License}
+    ${data.License}
 
-  ## Contributing
+    ## Contributing
 
-  ${data.Contributing}
-`;
+    ${data.Contributing}
+  
+    ## Creator
+  
+    ${data.Creator}
+
+    ## Contact-Me
+    * Name - ${data.Name}
+    * Email - ${data.Email}
+    * Github - ${data.Creator}
+    `;
 }
 
 module.exports = generateMarkdown;
